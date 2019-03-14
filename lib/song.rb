7db9bf7f4560d678204dfbdeb91e6b4efebd5c96
Serena Berry <siberry226@gmail.com>
2019-03-14 23:45:08 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+require 'pry'
+
+class Song
+  
+  attr_reader :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+  
+  def artists
+    @@artists.uniq
+  end
+  
+  def genres
+    @@genres.uniq
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    genre_count = {}
+    genres.each { |genre|
+      count = @@genres.count(genre)
+      genre_count[genre] = count }
+    genre_count
+  end
+    
+  def self.artist_count
+    artist_count = {}
+    artists.each { |artist|
+      count = @@artists.count(artist)
+      artist_count[artist] = count }
+    artist_count
+  end
+
+  
+end
